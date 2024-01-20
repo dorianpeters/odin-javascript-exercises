694fbeeb434cafabaa7194f04c6adf965cc57ea6
Dorian Peters <dpeters08@gmail.com>
2024-01-20 15:32:38 -0800

    Complete first two tests in removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeArgs) {
+  const filteredArray = array.filter(item => !removeArgs.includes(item));
+  console.log(array)
+  console.log(removeArgs)
+  console.log(filteredArray)
+  return filteredArray;
 };
 
+// removeFromArray([1,2,3,4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
