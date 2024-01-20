74f14e3aa10f75398efcbe750905b6989c149cbe
Dorian Peters <dpeters08@gmail.com>
2024-01-20 15:38:03 -0800

    finish removeFromArray excercise, shorten code.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,6 @@
 const removeFromArray = function(array, ...removeArgs) {
-  const filteredArray = array.filter(item => !removeArgs.includes(item));
-  console.log(array)
-  console.log(removeArgs)
-  console.log(filteredArray)
-  return filteredArray;
+  return array.filter(item => !removeArgs.includes(item));
 };
 
-// removeFromArray([1,2,3,4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
