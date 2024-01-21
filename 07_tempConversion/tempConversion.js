ad825ed3bdba5f9f4c91372fe999620654a55b9b
Dorian Peters <dpeters08@gmail.com>
2024-01-20 21:28:18 -0800

    Write both convert functions, both pass first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = (fTemp - 32) * (5/9);
+  cTempRounded = +cTemp.toFixed(1);
+  return cTempRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = cTemp * (9/5) + 32;
+  fTempRounded = +fTemp.toFixed(1);
+  return fTempRounded;
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
