fda35e0aa6b31650dc88518176dd08ef242d6273
Dorian Peters <dpeters08@gmail.com>
2024-01-21 13:06:02 -0800

    Write palindromes function, pass first test.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    filtered = filterString(inputString);
+    reversed = filtered.split('').reverse().join('');
+    if (filtered === reversed) return true;
+    else return false;
 
 };
 
+const filterString = function (input) { 
+    let result = '';
+    for (let i = 0; i < input.length; i++) {
+        const char = input[i]
+        if ((/[a-zA-Z0-9]/).test(char)) {
+            result += char;
+        }
+    }
+    return result;
+}
 // Do not edit below this line
 module.exports = palindromes;
