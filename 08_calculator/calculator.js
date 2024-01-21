a9a87a372445cb91554af555b7ef0c075fe4e981
Dorian Peters <dpeters08@gmail.com>
2024-01-20 21:43:28 -0800

    Complete multiply function and tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,13 +10,17 @@ const subtract = function(num1, num2) {
 const sum = function(numArray) {
   let total = 0;
   numArray.forEach(element => {
-    total+=element
+    total+=element;
   });
   return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let total = 1;
+  numArray.forEach(element => {
+    total*=element;
+  });
+  return total;
 };
 
 const power = function() {
