b0893635ac8a2162ba5167ecc4cf695dfb9ac200
Dorian Peters <dpeters08@gmail.com>
2024-02-18 11:46:01 -0800

    Finish findTheOldest exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  people.forEach(person => {
+    if (person.yearOfDeath === undefined) {
+      const today = new Date();
+      person.age = today.getFullYear() - person.yearOfBirth;
+    } else {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+  });
+  people.sort((a,b) => b.age - a.age);
+  console.table(people);
+  return people[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
